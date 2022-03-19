f29af2816ee88a334f488fa469e2bb203516ef4d
RamsesOrtiz <ramsesortiz.ar@gmail.com>
2022-03-19 11:54:37 -0300

    update first class

--- src/main/java/com/educacionit/spring/entidades/Alumno.java
+++ src/main/java/com/educacionit/spring/entidades/Alumno.java
@@ -1,4 +1,33 @@
 package com.educacionit.spring.entidades;
 
 public class Alumno {
+
+    private String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    private String documento;
+
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "descripcion='" + descripcion + '\'' +
+                ", documento='" + documento + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/educacionit/spring/principal/Principal.java
+++ src/main/java/com/educacionit/spring/principal/Principal.java
@@ -1,4 +1,32 @@
 package com.educacionit.spring.principal;
 
+import com.educacionit.spring.entidades.Alumno;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Principal {
+
+    private static final Logger log = LogManager.getLogger(Principal.class);
+
+    public static void main(String [] args){
+
+        try {
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+          //Alumno alumno = (Alumno) context.getBean("alumnoXMLA");
+            Alumno alumno = context.getBean("alumnoXML", Alumno.class);
+            alumno.setDescripcion("Alumno1");
+            alumno.setDocumento("01");
+            context.close();
+            log.info(alumno);
+        }catch (Exception e){
+            log.error(e);
+        }
+
+
+
+
+    }
 }
